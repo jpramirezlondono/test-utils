7c72aaeab945f6c3fdc6280f825d70975ada46f6
jpramirezlondono <jramirezlondono@integralads.com>
2024-06-06 16:32:58 -0500

    add analitycs

---
+++ filePayloadAnalyzer.py
@@ -0,0 +1,60 @@
+
+import sys
+import os
+import json
+import math
+import pandas as pd
+from dataclasses import dataclass
+
+
+MAX_RECORDS = 10
+
+@dataclass
+class File(object):
+    key: str
+    path: str
+
+def loadFile(prefix, file_name, sortKey):
+    thislist = []
+    try:
+        # request file name
+        f = open(file_name)
+        file_size = os.path.getsize(file_name)
+        data = json.load(f)
+        root_keys = list(data.keys())
+        print(root_keys)
+
+        keys_to_split = root_keys
+        # Split the object into smaller objects based on the keys
+        split_data = {key: data[key] for key in keys_to_split}
+        # Save each smaller object into a separate file
+        for key, value in split_data.items():
+            with open(f'{prefix}-{key}.json', 'w') as f:
+                df2 = pd.DataFrame(value)
+                df2[[sortKey]] = df2[[sortKey]].astype(int)
+                sorted = df2.sort_values(by=[sortKey])
+                sorted_data_list = sorted.to_dict(orient="records")
+                if( MAX_RECORDS != 0 ):
+                    sorted_data_list = sorted_data_list[:MAX_RECORDS]
+                sorted_data_list_pretty = json.dumps(sorted_data_list, indent=4)
+                if( MAX_RECORDS != 0 ):
+                    records = len(sorted_data_list)
+                else:
+                    records = len(df2.index)
+                f.write(sorted_data_list_pretty)
+                tfile_size = os.path.getsize(f'{prefix}-{key}.json')
+                print(f'{prefix}-{key} Records {records} size {tfile_size}')
+                fileRecord = File(key, f'{prefix}-{key}.json')
+                thislist.append(fileRecord)
+                #print(sorted_data_list_pretty)
+    except:
+        print(exception)
+        print('Error loading JSON file ... exiting')
+    return thislist
+
+
+fileListNemo = loadFile( "nemo", "/Users/jramirezlondono/Documents/response-nemo-1.json","pmCampaignId" )
+fileListGRPC = loadFile( "grpc", "/Users/jramirezlondono/Documents/response-rpc-1.json", "pmCampaignId" )
+print(str(fileListNemo))
+print(str(fileListGRPC))
+
--- json-splitter2.py
+++ json-splitter2.py
@@ -2,6 +2,51 @@ import sys
 import os
 import json
 import math
+def split (nameFile, data):
+    if isinstance(data, list):
+        data_len = len(data)
+        print(nameFile +' Valid JSON file found ' , data_len )
+    else:
+        print("JSON is not an Array of Objects")
+        exit()
+
+    # get numeric input
+    try:
+        mb_per_file = abs(float(4))
+    except:
+        print('Error entering maximum file size ... exiting')
+        exit()
+
+    # check that file is larger than max size
+    if file_size < mb_per_file * 1000000:
+        print('File smaller than split size, exiting')
+        exit()
+
+    # determine number of files necessary
+    num_files = math.ceil(file_size/(mb_per_file*1000000))
+    print('File will be split into',num_files,'equal parts')
+
+    # initialize 2D array
+    split_data = [[] for i in range(0,num_files)]
+
+    # determine indices of cutoffs in array
+    starts = [math.floor(i * data_len/num_files) for i in range(0,num_files)]
+    starts.append(data_len)
+
+    # loop through 2D array
+    for i in range(0,num_files):
+        # loop through each range in array
+        for n in range(starts[i],starts[i+1]):
+            split_data[i].append(data[n])
+
+        # create file when section is complete
+        name = os.path.basename(nameFile + file_name).split('.')[0] + '_' + str(i+1) + '.json'
+        with open(name, 'w') as outfile:
+            json.dump(split_data[i], outfile)
+
+        print('Part',str(i+1),'... completed')
+
+    print('Success! Script Completed')
 
 if sys.version_info[0] < 3:
     print('This script requires Python 3 or higher')
@@ -24,54 +69,12 @@ try:
     split_data = {key: data[key] for key in keys_to_split}
     # Save each smaller object into a separate file
     for key, value in split_data.items():
-        with open(f'tmp/nemo{key}.json', 'w') as f:
-            json.dump({key: value}, f)
-    
-    if isinstance(data, list):
-        data_len = len(data)
-        print('Valid JSON file found')
-    else:
-        print("JSON is not an Array of Objects")
-        exit()
+        with open(f'nemo-{key}.json', 'w') as f:
+            json.dump(value, f)
+            split(key, value)
 
 except:
     print('Error loading JSON file ... exiting')
     exit()
 
-# get numeric input
-try:
-    mb_per_file = abs(float(4))
-except:
-    print('Error entering maximum file size ... exiting')
-    exit()
 
-# check that file is larger than max size
-if file_size < mb_per_file * 1000000:
-    print('File smaller than split size, exiting')
-    exit()
-
-# determine number of files necessary
-num_files = math.ceil(file_size/(mb_per_file*1000000))
-print('File will be split into',num_files,'equal parts')
-
-# initialize 2D array
-split_data = [[] for i in range(0,num_files)]
-
-# determine indices of cutoffs in array
-starts = [math.floor(i * data_len/num_files) for i in range(0,num_files)]
-starts.append(data_len)
-
-# loop through 2D array
-for i in range(0,num_files):
-    # loop through each range in array
-    for n in range(starts[i],starts[i+1]):
-        split_data[i].append(data[n])
-    
-    # create file when section is complete
-    name = os.path.basename(file_name).split('.')[0] + '_' + str(i+1) + '.json'
-    with open(name, 'w') as outfile:
-        json.dump(split_data[i], outfile)
-        
-    print('Part',str(i+1),'... completed')
-
-print('Success! Script Completed')
